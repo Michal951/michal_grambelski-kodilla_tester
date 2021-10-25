98028fdd7ae15a2d345a5cb9c79573d0c1004f91
Michal Grambelski <m.grambelski95@wp.pl>
2021-10-25 20:23:56 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+         String myName = "Michał";
+         String myCity = "Malbork";
+         int numberOfMyGuitars = 3;
+         boolean ILikeBooks = true;
+         double heighs = 1.79;
+         int myYearOfBirth = 1995;
+         System.out.println("My name is " + myName);
+        System.out.println("I am from " + myCity);
+        System.out.println("I have a " + numberOfMyGuitars + " guitars");
+        System.out.println("I like reading books? " + ILikeBooks);
+        System.out.println(" My heigh is " + heighs);
+        System.out.println("I was born in " + myYearOfBirth);
+    }
+}
