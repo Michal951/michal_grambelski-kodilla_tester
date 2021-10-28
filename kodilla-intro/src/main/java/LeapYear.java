2a5baa3d3512ec6bdc836df4af9914fc130cd9a9
Michal Grambelski <m.grambelski95@wp.pl>
2021-10-28 21:51:02 +0200

    Initial version

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class LeapYear {
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2020;
+        int number1 = 4;
+        int number2 = 100;
+        int number3 = 400;
+        if (year%number1==0 && year%number2!=0 || year%number3==0) {
+            System.out.println("rok jest przestępny");
+        } else {
+            System.out.println("rok nie jest przestępny");
+        }
+    }
 }
--- kodilla-intro/src/main/java/Main.java
+++ kodilla-intro/src/main/java/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+    }
 }
